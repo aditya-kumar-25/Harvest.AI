c4fbe52d27e0978e069748d4e0c1effe9078044b
aditya-kumar-25 <aditya.pca.25@gmail.com>
2024-06-06 04:44:47 +0530

    UI-fixes

--- src/app/Components/Sidenav.tsx
+++ src/app/Components/Sidenav.tsx
@@ -15,29 +15,6 @@ type SidenavProps = {
   setChatOpened: any;
 };
 
-const sampleData = [
-  {
-    question: "What are the major crops grown in UAE?",
-    answer:
-      "The major crops grown in UAE include dates, tomatoes, cucumbers, eggplants, and lettuce.",
-  },
-  {
-    question: "What are the common farming practices in UAE?",
-    answer:
-      "Common farming practices in UAE include hydroponics, vertical farming, and greenhouse cultivation.",
-  },
-  {
-    question: "How does the UAE ensure water availability for agriculture?",
-    answer:
-      "The UAE utilizes advanced irrigation techniques such as drip irrigation and desalination of seawater to ensure water availability for agriculture.",
-  },
-  {
-    question: "What are the challenges faced by agriculture in UAE?",
-    answer:
-      "Challenges faced by agriculture in UAE include limited arable land, high temperatures, and water scarcity.",
-  },
-];
-
 const Sidenav: React.FC<SidenavProps> = ({ chatOpened, setChatOpened }) => {
   const [debounce, setDebounce] = useState<Boolean>(false);
   const [search, setSearch] = useState<string>("");
